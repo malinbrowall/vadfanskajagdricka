11310dc32580f25f48ef2fd8cb88bb50e156ead5
Simonjpersson <Siiimoon_@hotmail.com>
2016-03-22 10:15:17 +0100

    asdf
    asdas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 var SaveAll = []; 
 
 //När sidan laddas i webbläsaren körs ajax-anropet med GET metoden
-$(document).ready(function(){
+$(document).ready(function (){
         
         $.ajax({
 		method: 'GET',
@@ -25,25 +25,25 @@ $(document).ready(function(){
                  
 
 //funktion som körs när man klickar på knappen "sök"
-$('.btn').click(function(){
+$('.btn').click(function (){
 	//anropar API:et systemetapi genom ajax. hämtar ut alla, i JSON format. 
 		
 	
 		
 		//hämtar elementet "drink"
-		var x = document.getElementById("drink");
+		var x = document.getElementById('drink');
 		//skapar en randomfunktion som går mellan alla 0-49 objekt
 		var num = Math.floor((Math.random() * 49) + 1);
     
         //hämtar och sparar maträtten som användaren skrev in
-        var input = document.getElementById("input").value;
+        var input = document.getElementById('input').value;
         
     
         //variabel som sparar vilken typ utav dryck det är, används sedan till att matchas med en bild
         var type = (data[num].tags[0].name);
         
 		
-		x.innerHTML = "Till "+ input + " rekommenderar vi " + data[num].name + ". Den kostar bara " + data[num].price + "kr på systembolaget, och är ifrån " + data[num].country.name + ". FAN VAD GÖTTT <button class='save-btn'> Favorit </button>";
+		x.innerHTML = "Till " + input + " rekommenderar vi " + data[num].name + ". Den kostar bara " + data[num].price + "kr på systembolaget, och är ifrån " + data[num].country.name + ". FAN VAD GÖTTT <button class='save-btn'> Favorit </button>";
         //Beroende på vad "var type" får för värde(name) så visas ikonen kopplat till det värdet.
         //Inte världens snyggaste lösning men den funkar. Bilden visas och försvinner vid nästa sö
         //Var typ kan få 8 olika värden från vårt api anropp.
@@ -63,38 +63,39 @@ $('.btn').click(function(){
             document.getElementById("icon3").style.display = 'none'
         }; 
         if (type == 'rose'){
-            document.getElementById('icon4').style.display = 'block'
+            document.getElementById("icon4").style.display = 'block'
         } else {
-            document.getElementById('icon4').style.display = 'none'
+            document.getElementById("icon4").style.display = 'none'
         };
         if (type == 'cider'){
-            document.getElementById('icon5').style.display = 'block'
+            document.getElementById("icon5").style.display = 'block'
         } else {
-            document.getElementById('icon5').style.display = 'none'
+            document.getElementById("icon5").style.display = 'none'
         };
         if (type == 'lager'){
-            document.getElementById('icon6').style.display = 'block'
+            document.getElementById("icon6").style.display = 'block'
         } else {
-            document.getElementById('icon6').style.display = 'none'
+            document.getElementById("icon6").style.display = 'none'
         }
         if (type == 'white-wine'){
-            document.getElementById('icon7').style.display = 'block'
+            document.getElementById("icon7").style.display = 'block'
         } else {
-            document.getElementById('icon7').style.display = 'none'
+            document.getElementById("icon7").style.display = 'none'
         };
         if (type == 'sparkling'){
-            document.getElementById('icon8').style.display = 'block'
+            document.getElementById("icon8").style.display = 'block'
         } else {
-            document.getElementById('icon8').style.display = 'none'
+            document.getElementById("icon8").style.display = 'none'
         }
 
         //sparar i localstorage om användaren klickar på save knappen
-        $('.save-btn').on('click', function(){
+        $('.save-btn').on('click', function (){
         var saveDrink = data[num].name;
+        var saveKategori = type;
         var saveFood = input;
 
         
-        SaveAll.push({saveDrink,saveFood});
+        SaveAll.push({saveDrink,saveFood, saveKategori});
         var JSONFavorit = JSON.stringify(SaveAll);
         localStorage.setItem("Favorit", JSONFavorit);
         
@@ -113,14 +114,15 @@ $('.btn').click(function(){
     
      
     //körs nu när testknappen "se sparade" klickas på, i combo.html
-    $('.se-saved').click(function(){
+    $('.se-saved').click(function (){
     //hämtar från localstorage
     var favoriter = JSON.parse(localStorage.getItem("Favorit"));
    //printar ut listan för de sparade mat och dryckes kombinationerna
 	for (i = 0; i < favoriter.length; i++){
 		var dryck = favoriter[i].saveDrink;
 		var mat = favoriter[i].saveFood;
-		$("#fave").append("<p> Dryck: " + dryck + ". Mat: " + mat +"</p>");
+        var kate = favoriter[i].saveKategori;
+		$("#fave").append("<p>Dryck: " + kate + ". Namn: " + dryck + ". Mat: " + mat +"</p>");
 		$('.se-saved').hide();
 	}
 	});
